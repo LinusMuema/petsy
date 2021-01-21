9fe73d337ed6aa33aa196b37daa097e185a0b861
linus muema <linus.m.muema@gmail.com>
2021-01-22 00:39:26 +0300

    - confirmed database working

--- feature_dogs/src/main/java/com/moose/feature_dogs/DogsFragment.kt
+++ feature_dogs/src/main/java/com/moose/feature_dogs/DogsFragment.kt
@@ -1,13 +1,17 @@
 package com.moose.feature_dogs
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.moose.feature_dogs.databinding.FragmentDogsBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class DogsFragment : Fragment() {
@@ -17,7 +21,13 @@ class DogsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentDogsBinding.inflate(inflater, container, false)
-        viewModel.getFact()
+
+        lifecycleScope.launch {
+            viewModel.dogs.collect {
+                Log.d("Petsy", "onCreateView: $it")
+            }
+        }
+
         return binding.root
     }
 
