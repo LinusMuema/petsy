85d242596d3896044e27b4dcb6461ae72877de9f
linus muema <linus.m.muema@gmail.com>
2021-01-26 12:15:49 +0300

    - added FAB and motion layout

--- feature_dogs/src/main/java/com/moose/feature_dogs/presentation/DogsFragment.kt
+++ feature_dogs/src/main/java/com/moose/feature_dogs/presentation/DogsFragment.kt
@@ -1,17 +1,13 @@
 package com.moose.feature_dogs.presentation
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.moose.feature_dogs.R
 import com.moose.feature_dogs.databinding.FragmentDogsBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class DogsFragment : Fragment(R.layout.fragment_dogs) {
@@ -22,6 +18,15 @@ class DogsFragment : Fragment(R.layout.fragment_dogs) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentDogsBinding.bind(view)
+        val layout = binding.motionLayout
+
+        binding.fab.setOnClickListener {
+            layout.transitionToState(R.id.end)
+        }
+
+        binding.overlay.setOnClickListener {
+            layout.transitionToState(R.id.start)
+        }
 
         val manager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
         manager.gapStrategy = StaggeredGridLayoutManager.GAP_HANDLING_MOVE_ITEMS_BETWEEN_SPANS
